713e38c36e0497d910ca409381e89b96ce6bd832
Hein Tonny Køien <hein.tonny@koien.no>
2024-12-08 23:06:39 +0100

    Fix default example

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = "= 1.5"
+  required_version = "~> 1.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.13.0"
+      version = "~>4.13.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.3"
+      version = "~>3.6.3"
     }
   }
 }
